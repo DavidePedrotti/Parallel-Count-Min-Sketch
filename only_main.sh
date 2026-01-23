9aa0f669be0afdf9975d04116ff44a99c2c9921f
Gabardi <giorgia.gabardi@hpc-head-n1.unitn.it>
2026-01-23 18:06:27 +0100

    Update mainV2

---
+++ only_main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N cms_benchmark
+#PBS -l select=1:ncpus=8:mem=4gb
+#PBS -l walltime=0:30:00
+#PBS -q short_HPC4DS
+
+mkdir -p output error
+
+cd $PBS_O_WORKDIR
+
+module load mpich-3.2
+
+# Run benchmark
+mpirun.actual -np 2 ./main scripts/dataset_10000_ordered.txt
